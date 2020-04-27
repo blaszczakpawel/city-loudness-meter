dba3deec1b80c2b9a0ac4869478823b1a641f3d9
Quenoo <Miki10040@gmail.com>
2020-04-27 19:38:40 +0200

    Connect frontend files with server
    Added aditional Controller to return particulat .htm files.
    Moved "public" folder to Rest_Api/src/main/resources/static as Spring needs static files to be in this location.
    Changed 1 spring property to allow to return .htm files instead of .html

---
+++ Rest_Api/src/main/java/pl/pk/project/pz/sound_intensity/api/FrontController.java
@@ -0,0 +1,36 @@
+package pl.pk.project.pz.sound_intensity.api;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+
+@Controller
+public class FrontController {
+
+	@GetMapping("/")
+	public String returnIndex() {
+		return "index";
+	}
+	
+	@GetMapping("/about")
+	public String returnAbout() {
+		return "about";
+	}
+	
+	@GetMapping("/contribute")
+	public String returnContribute() {
+		return "contribute";
+	}
+	
+	@GetMapping("/APIGuide")
+	public String returnAPIGuide() {
+		return "APIGuide";
+	}
+	
+	@GetMapping("/error")
+	public String ReturnError() {
+		return "exampleError";
+	}
+
+}
