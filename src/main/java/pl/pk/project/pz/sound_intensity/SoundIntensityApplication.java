4cda5f46bfd9a9c13519585a2471d12b582bf3f9
Nexis4256 <Nexis4256@gmail.com>
2020-04-30 12:53:27 +0200

    No commits history Couldnt merge without conflicts 2 local repos so i ve done it manaully.

--- src/main/java/pl/pk/project/pz/sound_intensity/SoundIntensityApplication.java
+++ src/main/java/pl/pk/project/pz/sound_intensity/SoundIntensityApplication.java
@@ -2,12 +2,20 @@ package pl.pk.project.pz.sound_intensity;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootApplication
 public class SoundIntensityApplication{
 
     public static void main(String[] args) {
         SpringApplication.run(SoundIntensityApplication.class, args);
+    }
+
+    @Bean
+    public BCryptPasswordEncoder bCryptPasswordEncoder()
+    {
+        return new BCryptPasswordEncoder();
     }
 
 }
