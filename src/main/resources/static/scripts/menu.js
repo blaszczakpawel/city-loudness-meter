7038a930cacbb53c5114e62cd8aa9312f5acbdd7
domib22 <domib22@users.noreply.github.com>
2020-05-06 17:48:00 +0200

    Add menu bar

---
+++ src/main/resources/static/scripts/menu.js
@@ -0,0 +1,13 @@
+function menu()
+{
+    document.write( `
+        <div id="panel">
+            <ul id="menu">
+                <li><a href="/" data-title="Homepage - map"><img src="assets/homepage.svg" alt="Map"></a>
+                <li><a href="about" data-title="About us"><img src="assets/about.svg" alt="About"></a>
+                <li><a href="contribute" data-title="Contribute - Help us to collect data!"><img src="assets/networking.svg" alt="Contribute"></a>
+                <li><a href="APIGuide" data-title="Documentation"><img src="assets/document.svg" alt="Documentation"></a>
+            </ul>
+        </div>
+    `);
+}
