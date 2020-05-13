55502592c5801babdd9f18b28997655afc1d64b2
domib22 <domib22@users.noreply.github.com>
2020-05-13 17:39:38 +0200

    Correct design on map page

--- src/main/resources/static/scripts/menu.js
+++ src/main/resources/static/scripts/menu.js
@@ -2,6 +2,7 @@ function menu()
 {
     document.write( `
         <div id="panel">
+            <img src="assets/logo.svg">
             <ul id="menu">
                 <li><a href="/" data-title="Homepage - map"><img src="assets/homepage.svg" alt="Map"></a>
                 <li><a href="about" data-title="About us"><img src="assets/about.svg" alt="About"></a>
